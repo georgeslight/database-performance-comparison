19b3b8909784b7e4d2555c005cefa7576c7b69ce
georgeslight <georgeslight@gmail.com>
2025-01-18 11:50:41 +0100

    new benchmarks with seconds

--- benchmark_duckdb.py
+++ benchmark_duckdb.py
@@ -16,27 +16,31 @@ def monitor_resources(file_path, interval=1):
     """Monitor CPU, memory, and disk usage and write directly to a CSV file."""
     global monitoring
     
+    start_time = time.time()  # Record the start time
+    
     with open(file_path, 'a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["Timestamp", "CPU Usage (%)", "Memory Usage (%)", "Disk Usage (%)"])
+        writer.writerow(["Elapsed Seconds", "CPU Usage (%)", "Memory Usage (%)", "Disk Usage (%)"])
         
         while monitoring:
-            start = time.time()
+            iteration_start = time.time()
+
+            # Calculate elapsed time in seconds
+            elapsed_seconds = iteration_start - start_time
+
             # Get memory status
             memory = psutil.virtual_memory().percent
             # Get CPU usage
             cpu = psutil.cpu_percent(interval=None)
             # Get Disk usage
             disk = psutil.disk_usage('/').percent
-            # Execution time
-            timestamp = time.time()
 
             # Write metrics
-            writer.writerow([timestamp, cpu, memory, disk])
+            writer.writerow([round(elapsed_seconds, 2), cpu, memory, disk])
             file.flush()  # Ensure data is written to file
 
             # Ensure consistent intervals
-            elapsed = time.time() - start
+            elapsed = time.time() - iteration_start
             if elapsed < interval:
                 time.sleep(interval - elapsed)
 
@@ -53,7 +57,7 @@ def execute_query():
                 query = query_file.read()
 
             # Start the monitoring thread
-            monitor_thread = threading.Thread(target=monitor_resources, args=("resource_usage_duck.csv", 1))
+            monitor_thread = threading.Thread(target=monitor_resources, args=("resource_usage_duck_1.csv", 1))
             monitor_thread.start()
 
             # Execute the query and measure time
